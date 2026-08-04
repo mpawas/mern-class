ee7e52effd464339338ed2468d73e5281f351230
Pawas Mishra <mishrapawas@Pawass-MacBook-Pro.local>
2026-08-04 06:48:23 +0545

    Update someRandom exception module

--- backend/src/modules/exception/someRandom.ts
+++ backend/src/modules/exception/someRandom.ts
@@ -1,14 +1,9 @@
 import HttpException from './httpException';
 
 class RandomError extends HttpException {
-  public status: number;
-  public code: number | string;
-  public message: string;
-  constructor(status: number, message: string, code: 'random.string') {
-    this.code = code;
-    this.status = status;
-    this.message = message;
+  constructor(status: number, message: string) {
+    super(status, message, 'random.string');
   }
 }
 
-export default HttpException;
+export default RandomError;
